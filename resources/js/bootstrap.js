bffd7a75c8e4ad7b0a9be10c12367d885defb883
Hijazi MBA M1 <hijazi@irekasoft.com>
2024-11-02 00:47:33 +0800

    --

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,8 +11,6 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-window.axios.defaults.withCredentials = true;
-window.axios.defaults.withXSRFToken = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
